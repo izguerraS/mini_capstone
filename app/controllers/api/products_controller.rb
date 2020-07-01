07bccd69bdfe1a4677d1c99edcbc1cd6a3196453
Sam <Izguerra.sam@gmail.com>
2020-06-30 20:15:40 -0500

    json and views added

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,9 +1,27 @@
 class Api::ProductsController < ApplicationController
 
     def name
+        @name = Product[0]["name"]
         
 
         render 'name_information.json.jb'
     end
 
+    def price
+        @price = Product[0]["price"]
+
+        render 'price_information.json.jb'
+    end
+
+    def image_url
+        @image_url = Product[0]["image_url"]
+
+        render 'image_url_information.json.jb'
+    end
+
+    def description
+        @description = Product[0]["description"]
+
+        render 'description_information.json.jb'
+    end
 end
