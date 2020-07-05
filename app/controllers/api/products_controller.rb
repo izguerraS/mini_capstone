9b0768d2a0ab2eb8b6e6b072d75cc5ecfca0d0ae
Sam <Izguerra.sam@gmail.com>
2020-07-05 10:49:51 -0500

    show, create, update, delete  ethods to date

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,10 +1,40 @@
 class Api::ProductsController < ApplicationController
 
-    def index
-    @product = Product.all
+# def index
+#   @products = Product.all
 
-    render 'index.json.jb'
+   
+def show
+  @product = Product.find_by(name: params[:name])
+ render 'show.json.jb'
 end
+
+
+def create
+  @product = Product.new(
+    name: params[:name]
+  )
+  @product.save
+  render 'show.json.jb'
+end
+
+
+def update
+  @product = Product.find_by(id: params[:name])
+
+  @product.name = "Toshiba"
+ #update particular product
+ render 'index.json.jb'
+ end
+
+
+#  def destroy
+#   @product = Product.find_by(id: params[:id])
+#   @product.destroy
+#   render json {message: "you have successfully deleted the recipe"}
+#  end
+end 
+ 
 
     
-end
+
--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,9 @@ Rails.application.routes.draw do
     namespace :api do
 
     get "/products" => "products#index"
+    get "/products/:id" => "products#show"
+    post "/products/:id" => "products#update"
+    #delete '/products/:id' => "products#destroy"
    
   end
-
-
-
-
 end
