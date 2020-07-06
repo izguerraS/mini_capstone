5fd96a882bf8461e1cdcac33c0a753c928b29b21
Sam <Izguerra.sam@gmail.com>
2020-07-06 16:36:27 -0500

    destroy method successful

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -28,4 +28,10 @@ class Api::ProductsController < ApplicationController
   @product.save
   render 'show.json.jb'
  end
+
+ def destroy
+  @product = Product.find_by(id: params[:id])
+  @product.destroy
+  render 'destroy.json.jb'
+ end
 end
