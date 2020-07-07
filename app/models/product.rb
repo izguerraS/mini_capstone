6680b2c770d25fe5eaca0dc4ac9effc03010c63e
Sam <Izguerra.sam@gmail.com>
2020-07-06 20:42:35 -0500

    3/4 of models complete

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,20 @@
 class Product < ApplicationRecord
+  
+  
+  def is_discounted
+    if price < 10
+      "true"
+    else
+      "false"
+  end
+end
+
+  def product_tax
+    price * 0.09
+ end
+
+
+def total
+ price + product_tax
+ end
 end
