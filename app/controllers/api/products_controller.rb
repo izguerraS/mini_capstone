8145a3b69430a95caeeecbefd81b6eb9fc30bd82
Sam <Izguerra.sam@gmail.com>
2020-07-09 19:48:44 -0500

    added search in controller

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,7 +1,12 @@
 class Api::ProductsController < ApplicationController
 
  def index
-   @products = Product.all
+  #  @products = Product.all
+  if params[:search]
+    @products = Product.where("description LIKE ?", "%#{params[:search]}%")
+  else
+    @products = Product.all
+  end
    render 'index.json.jb'
  end
 
@@ -22,6 +27,7 @@ class Api::ProductsController < ApplicationController
   else
     render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
   end
+end
 
  def update
   @product = Product.find_by(id: params[:id])
@@ -33,12 +39,14 @@ class Api::ProductsController < ApplicationController
     render 'show.json.jb'
   else 
   render 'show.json.jb'
+  end
  end
 
  def destroy
   @product = Product.find_by(id: params[:id])
   @product.destroy
   render 'destroy.json.jb'
- end
- end
+  end
 end
+
+
